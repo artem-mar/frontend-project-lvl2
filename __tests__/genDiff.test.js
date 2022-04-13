e1b56580319d4149b3c0be29485bc160cd4c1e2b
Artem M <artemmarin91@mail.ru>
2022-04-14 00:02:21 +0300

    + fixtures

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,5 +4,5 @@ import genDiff from '../src/genDiff';
 test('reverse', () => {
   const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
 
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(result);
+  expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')).toBe(result);
 });
