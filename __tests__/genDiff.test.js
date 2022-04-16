943feee8e3c2a18a58cbc410d0426f02a865544a
Artem M <artemmarin91@mail.ru>
2022-04-16 21:05:38 +0300

    added test yaml

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-undef */
 import genDiff from '../src/genDiff';
 
-test('reverse', () => {
+test('genDiff', () => {
   const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
 
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(result);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml')).toBe(result);
 });
