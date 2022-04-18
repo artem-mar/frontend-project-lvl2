5731a3c097e75522ed37826c255247866291baae
Artem M <artemmarin91@mail.ru>
2022-04-18 21:51:59 +0300

    upd test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,8 +2,52 @@
 import genDiff from '../src/genDiff';
 
 test('genDiff', () => {
-  const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
-
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(result);
-  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml')).toBe(result);
+  const expected = [
+    '{',
+    '    common: {',
+    '      + follow: false',
+    '        setting1: Value 1',
+    '      - setting2: 200',
+    '      - setting3: true',
+    '      + setting3: null',
+    '      + setting4: blah blah',
+    '      + setting5: {',
+    '            key5: value5',
+    '        }',
+    '        setting6: {',
+    '            doge: {',
+    '              - wow: ',
+    '              + wow: so much',
+    '            }',
+    '            key: value',
+    '          + ops: vops',
+    '        }',
+    '    }',
+    '    group1: {',
+    '      - baz: bas',
+    '      + baz: bars',
+    '        foo: bar',
+    '      - nest: {',
+    '            key: value',
+    '        }',
+    '      + nest: str',
+    '    }',
+    '  - group2: {',
+    '        abc: 12345',
+    '        deep: {',
+    '            id: 45',
+    '        }',
+    '    }',
+    '  + group3: {',
+    '        deep: {',
+    '            id: {',
+    '                number: 45',
+    '            }',
+    '        }',
+    '        fee: 100500',
+    '    }',
+    '}',
+  ];
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(expected.join('\n'));
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml')).toBe(expected.join('\n'));
 });
