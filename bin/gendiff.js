c0b0fe29927e0876e47978c49955827829c63f2a
Artem M <artemmarin91@mail.ru>
2022-04-18 21:52:18 +0300

    upd gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((path1, path2) => console.log(genDiff(path1, path2)));
 
 program.parse();
