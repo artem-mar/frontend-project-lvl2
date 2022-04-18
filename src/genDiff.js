207f63e9fc6bcbf8a745277fcf5de5a5ddfbe66f
Artem M <artemmarin91@mail.ru>
2022-04-18 21:53:03 +0300

    upd genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,28 +1,13 @@
-import _ from 'lodash';
 import parse from './parse.js';
+import compare from './compare.js';
+import stylish from './stylish.js';
 
-const genDiff = (path1, path2) => {
+const genDiff = (path1, path2, style = stylish) => {
   const file1 = parse(path1);
-  const keys1 = Object.keys(file1);
   const file2 = parse(path2);
-  const keys2 = Object.keys(file2);
+  const differences = compare(file1, file2);
 
-  const allSortedUniqKeys = _.sortedUniq(_.union(keys1, keys2).sort());
-
-  const resultArray = allSortedUniqKeys.reduce((acc, key) => {
-    if (keys1.includes(key) && keys2.includes(key)) {
-      return file1[key] === file2[key]
-        ? [...acc, `   ${key}: ${file1[key]}`]
-        : [...acc, ` - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}`];
-    }
-    if (keys1.includes(key) && !keys2.includes(key)) {
-      return [...acc, ` - ${key}: ${file1[key]}`];
-    }
-
-    return [...acc, ` + ${key}: ${file2[key]}`];
-  }, []);
-  const resultString = _.join(resultArray, '\n');
-  return `{\n${resultString}\n}`;
+  return style(differences);
 };
 
 export default genDiff;
