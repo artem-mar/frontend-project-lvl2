a56d8b51df816c25358254c9e1c2d39f6741828d
Artem M <artemmarin91@mail.ru>
2022-04-20 03:25:35 +0300

    + plain.js

---
+++ formatters/plain.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+
+const plain = (diff) => {
+  const iter = (diffs, pat = undefined) => {
+    const lines = diffs.map((item) => {
+      const path = pat ? `${pat}.${item.key}` : item.key;
+      // тут мы добиваемся того, чтоб строки были в кавычках, а [complex] и boolean - без
+      let value;
+      if (_.isObject(item.value)) {
+        value = '[complex value]';
+      } else if (typeof item.value === 'string') {
+        value = `'${item.value}'`;
+      } else {
+        value = item.value;
+      }
+
+      let string;
+      switch (item.type) {
+        case 'added': {
+          string = `\nProperty '${path}' was added with value: ${value}`;
+          break;
+        }
+        case 'removed': {
+          string = `\nProperty '${path}' was removed`;
+          break;
+        }
+        case 'updRemoved': {
+          string = `\nProperty '${path}' was updated. From ${value} to `;
+          break;
+        }
+        case 'updAdded': {
+          string = `${value}`;
+          break;
+        }
+        case 'comparison object': {
+          string = iter(item.value, path);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+      return string;
+    });
+    return lines.join('');
+  };
+  return iter(diff);
+};
+
+export default plain;
