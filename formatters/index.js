ab3a26e23ae8a4b2ac648910e3741504f2255b99
Artem M <artemmarin91@mail.ru>
2022-04-20 20:38:46 +0300

    added json formatter

--- formatters/index.js
+++ formatters/index.js
@@ -1,10 +1,13 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import toJSON from './json.js';
 
 const chooseFormat = (format) => {
   let formatter;
   if (format === 'plain') {
     formatter = plain;
+  } else if (format === 'json') {
+    formatter = toJSON;
   } else {
     formatter = stylish;
   }
---
+++ formatters/json.js
@@ -0,0 +1,3 @@
+const toJSON = (diffs) => JSON.stringify(diffs);
+
+export default toJSON;
