90e78eb05e4bde43c81f038daaaad0088f519743
Artem M <artemmarin91@mail.ru>
2022-04-20 20:38:25 +0300

    added test json

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -71,3 +71,60 @@ test('genDiff plain', () => {
   expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml', 'plain'))
     .toBe(`\n${expected.join('\n')}`);
 });
+
+test('genDiff json', () => {
+  /* подозреваю, что эта проверка - какая-то фигня, т.к. тут фактически проверяется работа
+  форматтера json с помощью самого себя, ведь он состоит из лдной строчки JSON.stringify(...) */
+  const expected = [
+    {
+      key: 'common',
+      value: [
+        { key: 'follow', value: false, type: 'added' },
+        { key: 'setting1', value: 'Value 1', type: 'equal' },
+        { key: 'setting2', value: 200, type: 'removed' },
+        { key: 'setting3', value: true, type: 'updRemoved' },
+        { key: 'setting3', value: null, type: 'updAdded' },
+        { key: 'setting4', value: 'blah blah', type: 'added' },
+        { key: 'setting5', value: { key5: 'value5' }, type: 'added' },
+        {
+          key: 'setting6',
+          value: [
+            {
+              key: 'doge',
+              value: [
+                { key: 'wow', value: '', type: 'updRemoved' },
+                { key: 'wow', value: 'so much', type: 'updAdded' },
+              ],
+              type: 'comparison object',
+            },
+            { key: 'key', value: 'value', type: 'equal' },
+            { key: 'ops', value: 'vops', type: 'added' },
+          ],
+          type: 'comparison object',
+        },
+      ],
+      type: 'comparison object',
+    },
+    {
+      key: 'group1',
+      value: [
+        { key: 'baz', value: 'bas', type: 'updRemoved' },
+        { key: 'baz', value: 'bars', type: 'updAdded' },
+        { key: 'foo', value: 'bar', type: 'equal' },
+        { key: 'nest', value: { key: 'value' }, type: 'updRemoved' },
+        { key: 'nest', value: 'str', type: 'updAdded' },
+      ],
+      type: 'comparison object',
+    },
+    { key: 'group2', value: { abc: 12345, deep: { id: 45 } }, type: 'removed' },
+    {
+      key: 'group3',
+      value: { deep: { id: { number: 45 } }, fee: 100500 },
+      type: 'added',
+    },
+  ];
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json'))
+    .toBe(JSON.stringify(expected));
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml', 'json'))
+    .toBe(JSON.stringify(expected));
+});
