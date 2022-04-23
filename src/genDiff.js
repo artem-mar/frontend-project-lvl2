852f4ef2f711fcddeec8d169480fb53d37060fd9
Artem M <artemmarin91@mail.ru>
2022-04-23 07:09:37 +0300

    rf gendiff & genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,10 +1,11 @@
-import parser from './parser.js';
+import parse from './parser.js';
 import compare from './compare.js';
-import chooseFormat from '../formatters/index.js';
+import readFile from './readFile.js';
+import chooseFormat from './formatters/index.js';
 
 const genDiff = (path1, path2, formatName = 'stylish') => {
-  const file1 = parser(path1);
-  const file2 = parser(path2);
+  const file1 = parse(readFile(path1));
+  const file2 = parse(readFile(path2));
   const differences = compare(file1, file2);
   const formatter = chooseFormat(formatName);
 
