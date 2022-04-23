b3a53bcd4642d35b35ba4dfbc58ff3ee84514f05
Artem M <artemmarin91@mail.ru>
2022-04-23 22:14:04 +0300

    fix genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,12 +1,11 @@
-import parse from './parser.js';
 import compare from './compare.js';
-import readFile from './readFile.js';
+import getData from './parsers.js';
 import chooseFormat from './formatters/index.js';
 
 const genDiff = (path1, path2, formatName = 'stylish') => {
-  const file1 = parse(readFile(path1));
-  const file2 = parse(readFile(path2));
-  const differences = compare(file1, file2);
+  const file1Data = getData(path1);
+  const file2Data = getData(path2);
+  const differences = compare(file1Data, file2Data);
   const formatter = chooseFormat(formatName);
 
   return formatter(differences);
